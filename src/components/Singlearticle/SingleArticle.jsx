51e7f7f7c1eb36377dfb0c719a35bdd8012c0f1c
Enamul Ali <enamulali@Enamuls-MacBook-Pro.local>
2022-06-14 15:39:33 +0100

    create single article route

---
+++ src/components/Singlearticle/SingleArticle.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchArticleById } from "../../utils/api";
+
+const SingleArticle = () => {
+  const [singleArticle, setSingleArticle] = useState({});
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchArticleById(id).then((articleFromApi) => {
+      setSingleArticle(articleFromApi);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2>{singleArticle.title}</h2>
+      <p>{singleArticle.body}</p>
+      <h4>Written by: {singleArticle.author}</h4>
+      <p>{singleArticle.created_at}</p>
+      <p>Votes: {singleArticle.votes}</p>
+      <p>Comments: {singleArticle.comment_count}</p>
+    </>
+  );
+};
+
+export default SingleArticle;
