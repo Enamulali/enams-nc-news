8e35b201c9579f1e41d3ceb280181b4f258a7c75
Enamul Ali <enamulali@enamuls-mbp.home>
2022-06-17 10:24:36 +0100

    remove guest liking

--- src/components/Votes/Votes.jsx
+++ src/components/Votes/Votes.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { patchArticleVotes } from "../../utils/api";
+import { UserContext } from "../../contexts/User";
 import "./Votes.css";
+import { BiLike } from "react-icons/bi";
 
 const Votes = ({ votes, articleId }) => {
   //creating a new votes state for optimistic rendering
   const [voteInc, setVoteInc] = useState(0);
+
+  //useContext to disable guest from liking an article
+  const { loggedInUser } = useContext(UserContext);
 
   //everytime like button pressed, state is incremented and patch is sent
   const handleClick = () => {
@@ -22,11 +27,17 @@ const Votes = ({ votes, articleId }) => {
   //onClick incVotes and then disable after voteInc > 0
   return (
     <>
-      <p>Votes: {votes + voteInc}</p>
+      <div>
+        <p className="comments-subheading">Votes: {votes + voteInc}</p>
 
-      <button className="like-btn" onClick={handleClick} disabled={voteInc > 0}>
-        Like
-      </button>
+        <button
+          className="like-btn"
+          onClick={handleClick}
+          disabled={voteInc > 0 || loggedInUser.name === "Guest"}
+        >
+          <BiLike className="article-icon" /> Like
+        </button>
+      </div>
     </>
   );
 };
