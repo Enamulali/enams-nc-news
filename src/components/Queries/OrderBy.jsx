e2159414cef1847b3f5ebc81ff309871cf860fb8
Enamul <enamul97@icloud.com>
2022-08-11 12:29:07 +0100

    filter container styling fix

--- src/components/Queries/OrderBy.jsx
+++ src/components/Queries/OrderBy.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  AiOutlineSortAscending,
+  AiOutlineSortDescending,
+} from "react-icons/ai";
 
 const OrderBy = ({ setOrderByValue, setSearchTerm, sortByValue }) => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  console.log(width);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const handleAscClick = (e) => {
     setSearchTerm({ sort_by: sortByValue, order: e.target.value });
     setOrderByValue(e.target.value);
@@ -13,12 +33,14 @@ const OrderBy = ({ setOrderByValue, setSearchTerm, sortByValue }) => {
 
   return (
     <>
-      <button onClick={handleAscClick} value="asc" className="order-btn">
-        ASC
-      </button>
-      <button onClick={handleDescClick} value="desc" className="order-btn">
-        DESC
-      </button>
+      <div className="order-btn-container">
+        <button onClick={handleAscClick} value="asc" className="order-btn">
+          {width > 500 ? "ASC" : <AiOutlineSortAscending />}
+        </button>
+        <button onClick={handleDescClick} value="desc" className="order-btn">
+          {width > 500 ? "DESC" : <AiOutlineSortDescending />}
+        </button>
+      </div>
     </>
   );
 };
