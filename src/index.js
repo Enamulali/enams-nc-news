9172a72502b8d88bb54fac9a8e339250b7cd7b3f
Enamul <enamul97@icloud.com>
2022-08-11 12:28:52 +0100

    uninstall bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
