eb80d5f8c90934aba2092b16be0858e7f307a505
Enamulali <enamul97@icloud.com>
2023-02-26 21:23:06 +0000

    Home articles styling & pagination

--- src/components/Home/ArticleAuthors.jsx
+++ src/components/Home/ArticleAuthors.jsx
@@ -1,35 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Expandable from "../Expandable/Expandable";
 import "./Home.css";
 
 const ArticleAuthors = ({ articles, users }) => {
+  const [visibleArticles, setVisibleArticles] = useState(3);
+
   return (
     <div className="user-articles-container">
-      <h2 className="home-heading">View Articles by Author:</h2>
+      <h2 className="home-heading">View Latest Articles by Author:</h2>
       <Expandable buttontext={"Show"}>
         <ul className="user-articles-ul">
           {users.map((user) => {
+            const userArticles = articles.filter(
+              (article) => article.author === user.username
+            );
             return (
               <li className="user-articles-li" key={user.username}>
-                <h5>{user.name}</h5>
-                <img
-                  className="user-img"
-                  src={user.avatar_url}
-                  alt={user.name}
-                />
+                <div className="user-info">
+                  <img
+                    className="user-img"
+                    src={user.avatar_url}
+                    alt={user.name}
+                  />
+                  <h5>{user.name}</h5>
+                </div>
                 <Expandable buttontext={"Show"}>
-                  {articles.map((article) => {
+                  {userArticles.slice(0, visibleArticles).map((article) => {
                     return (
                       <Link
                         to={`articles/id/${article.article_id}`}
                         key={article.article_id}
                       >
-                        <p>
-                          {{ ...article }.author === { ...user }.username
-                            ? article.title
-                            : null}
-                        </p>
+                        <p>{article.title}</p>
                       </Link>
                     );
                   })}
